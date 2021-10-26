7909ec662c50a6b017eef0daf7dcdcc36684e0ab
Tammy Lam <lamtammy1515@gmail.com>
2021-10-26 17:15:02 -0500

    made new files on the client side

--- contact_api/db/seeds.rb
+++ contact_api/db/seeds.rb
@@ -10,6 +10,12 @@ business = Category.create(name: 'business')
 family = Category.create(name: 'family')
 friend = Category.create(name: 'friend')
 
-Contact.create(name: 'sunshine', email: 'sunshine@example.com', number: "(111)111-1111")
-Contact.create(name: 'mick', email: 'mick@example.com', number: "(222)222-2222")
-Contact.create(name: 'luffy', email: 'luffy@example.com', number: "(333)333-3333")
+# Contact.create(name: 'sunshine', email: 'sunshine@example.com', number: "(111)111-1111")
+# Contact.create(name: 'mick', email: 'mick@example.com', number: "(222)222-2222")
+# Contact.create(name: 'luffy', email: 'luffy@example.com', number: "(333)333-3333")
+
+Contact.create([
+    {name: 'sunshine', email: 'sunshine@example.com', number: "(111)111-1111", category_id: 1},
+    {name: 'mick', email: 'mick@example.com', number: "(222)222-2222", category_id: 2},
+    {name: 'luffy', email: 'luffy@example.com', number: "(333)333-3333", category_id: 3}
+])
