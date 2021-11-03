8538b7a4748dc51d224878810158bf43e4091d63
Tammy Lam <lamtammy1515@gmail.com>
2021-11-03 09:25:50 -0500

    Pushing all code to github

--- contacts_client/index.js
+++ contacts_client/index.js
@@ -1,4 +1,6 @@
 // any global variables 
-const base_url = "//127.0.0.1:3000"
+const base_url = `http://127.0.0.1:3000`
 const contactService = new ContactService(base_url)
+
+contactService.getContacts()
 //any initializations of the application 
--- contacts_client/src/contactService.js
+++ contacts_client/src/contactService.js
@@ -1,1 +1,19 @@
 // make all service calls regarding a contact object
+
+
+class ContactService {
+
+    constructor(endpoint) {
+        this.endpoint = endpoint
+    }
+
+    // 1: read/index action 
+
+    getContacts(){
+         fetch (`${this.endpoint}/contacts`)
+         .then(resp => resp.json())
+         .then()
+    }
+
+   
+}
