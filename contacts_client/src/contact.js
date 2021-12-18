4e9bd3ebff58c3c795c74e589f4f7f96d0c32280
tammylam <lamtammy1515@gmail.com>
2021-12-18 11:28:25 -0600

    created state and new li element in contacts class

--- contacts_client/src/contact.js
+++ contacts_client/src/contact.js
@@ -3,8 +3,16 @@ class Contact {
     static all = []
 
     constructor(id, name, number, email, category_id){
+        this.id = id
+        this.name = name
+        this.number = number 
+        this.email = email 
+        this.category_id = category_id
 
-
+        this.element = document.createElement('li')
+        this.element.dataset.id = this.id
+        this.element.id = `contact-${this.id}`
+        
         Contact.all.push(this)
     }
 }
