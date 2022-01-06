2cc6cc5dcdb311d4438013c0bd25c4c1b5d6c9fa
tammylam <lamtammy1515@gmail.com>
2022-01-06 11:05:16 -0600

    adding an event listener to contact form

--- contacts_client/index.js
+++ contacts_client/index.js
@@ -1,8 +1,8 @@
 // any global variables 
 const base_url = `http://127.0.0.1:3000`
 const contactService = new ContactService(base_url)
+Contact.contactForm.addEventListener('submit', handleSubmit())
 
 contactService.getContacts()
-//any initializations of the application 
-//console.log(Contact.all)
 Contact.renderForm()
+
