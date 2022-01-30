245a73d04cc10afdc9cd4859aa72d6cd51e52939
tammylam <lamtammy1515@gmail.com>
2022-01-29 23:12:23 -0600

    instantiated deleteBtn

--- contacts_client/index.js
+++ contacts_client/index.js
@@ -1,6 +1,7 @@
 // any global variables 
 const base_url = `http://127.0.0.1:3000`
 const contactService = new ContactService(base_url)
+const deleteBtn = document.getElementById('delete-btn')
 Contact.contactForm.addEventListener('submit', handleSubmit())
 
 contactService.getContacts()
