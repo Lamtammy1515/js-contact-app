fb8959f86887479ae412284b114d20d3b678348e
tammylam <lamtammy1515@gmail.com>
2022-02-07 22:25:48 -0600

    unable to fix issue

--- contacts_client/index.js
+++ contacts_client/index.js
@@ -1,11 +1,11 @@
 // // any global variables 
-// const base_url = `http://127.0.0.1:3000`
-// const contactService = new ContactService(base_url)
+ const base_url = `http://127.0.0.1:3000`
+ const contactService = new ContactService(base_url)
 // const deleteBttn = document.getElementById('delete-bttn')
 // deleteBttn.addEventListener('click', )
 // Contact.contactForm.addEventListener('submit', handleSubmit())
 
-// contactService.getContacts()
+ contactService.getContacts()
 // Contact.renderForm()
 
 // function handleSubmit(event){
--- contacts_client/src/contactService.js
+++ contacts_client/src/contactService.js
@@ -1,5 +1,20 @@
+class ContactService{
 
+    constructor(endpoint){
+        this.endpoint = endpoint
+    }
 
+    // 1: read/index action 
+
+    getContacts(){
+        fetch(`${this.endpoint}/contacts`)
+        .then(resp => resp.json())
+        .then(contacts => {
+            debugger
+        })
+    }
+    
+}
 
 
 // make all service calls regarding a contact object
